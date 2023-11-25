15d660f1a4a46eff0bcd7215cb2a4556fb663273
kithib <1010465183@qq.com>
2023-11-25 20:54:29 +0800

    new file:   memory/data/__init__.py 	new file:   memory/data/chatglmRequest.py

---
+++ memory/data/chatglmRequest.py
@@ -0,0 +1,121 @@
+"""
+Author: kit
+
+File: chatglmRequest.py
+Description: Wrapper functions for chatglm.
+"""
+import json
+import random
+import time 
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers.generation.utils import GenerationConfig
+#import openai
+#from utils import *
+#openai.api_key = openai_api_key
+'''
+#使用chatGPT
+def ChatGPT_request(prompt): 
+  """
+  Given a prompt and a dictionary of GPT parameters, make a request to OpenAI
+  server and returns the response. 
+  ARGS:
+    prompt: a str prompt
+    gpt_parameter: a python dictionary with the keys indicating the names of  
+                   the parameter and the values indicating the parameter 
+                   values.   
+  RETURNS: 
+    a str of GPT-3's response. 
+  """
+  # temp_sleep()
+  try: 
+    completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo", 
+    messages=[{"role": "user", "content": prompt}]
+    )
+    return completion["choices"][0]["message"]["content"]
+  
+  except: 
+    print ("ChatGPT ERROR")
+    return "ChatGPT ERROR"
+'''
+#使用chatglm
+def init_model():
+  """
+  init chatglm3 model in local
+  """
+  model = AutoModelForCausalLM.from_pretrained(
+    "/data/tds/ChatGLM3/chatglm3-6b",
+    torch_dtype=torch.float16,
+    device_map="cuda:2",
+    trust_remote_code=True
+  )
+  tokenizer = AutoTokenizer.from_pretrained(
+    "/data/tds/ChatGLM3/chatglm3-6b",
+    use_fast=False,
+    trust_remote_code=True
+  )
+  return model, tokenizer
+def Chatglm_request(prompt,model,tokenizer): 
+  """
+  Given a prompt and a dictionary of chatglm3 parameters, make a request and returns the response. 
+  ARGS:
+    prompt: a str prompt
+    model: init_model() return model paremater
+    tokenizer: init_model() return tokenizer method
+  RETURNS: 
+    a str of chatglm's response. 
+  """
+  # temp_sleep()
+  try: 
+    response, history = model.chat(tokenizer, prompt, history=[])
+    return response , history
+  except: 
+    print ("chatglm ERROR")
+    return "chatglm ERROR"
+  
+
+if __name__ == "__main__":
+  model , tokenizer = init_model()
+  prompt = """
+---
+Character 1: Maria Lopez is working on her physics degree and streaming games on Twitch to make some extra money. She visits Hobbs Cafe for studying and eating just about everyday.
+Character 2: Klaus Mueller is writing a research paper on the effects of gentrification in low-income communities.
+
+Past Context: 
+138 minutes ago, Maria Lopez and Klaus Mueller were already conversing about conversing about Maria's research paper mentioned by Klaus This context takes place after that conversation.
+
+Current Context: Maria Lopez was attending her Physics class (preparing for the next lecture) when Maria Lopez saw Klaus Mueller in the middle of working on his research paper at the library (writing the introduction).
+Maria Lopez is thinking of initating a conversation with Klaus Mueller.
+Current Location: library in Oak Hill College
+
+(This is what is in Maria Lopez's head: Maria Lopez should remember to follow up with Klaus Mueller about his thoughts on her research paper. Beyond this, Maria Lopez doesn't necessarily know anything more about Klaus Mueller) 
+
+(This is what is in Klaus Mueller's head: Klaus Mueller should remember to ask Maria Lopez about her research paper, as she found it interesting that he mentioned it. Beyond this, Klaus Mueller doesn't necessarily know anything more about Maria Lopez) 
+
+Here is their conversation. 
+
+Maria Lopez: "
+---
+Output the response to the prompt above in json. The output should be a list of list where the inner lists are in the form of ["<Name>", "<Utterance>"]. Output multiple utterances in ther conversation until the conversation comes to a natural conclusion.
+Example output json:
+{"output": "[["Jane Doe", "Hi!"], ["John Doe", "Hello there!"] ... ]"}
+"""
+  respond = ""
+  while(len(respond) < 100):
+    respond , _ = Chatglm_request(prompt , model ,tokenizer)
+  print(respond)
+  
+  #print (ChatGPT_request(prompt))
+
+
+
+
+
+
+
+
+
+
+
+
